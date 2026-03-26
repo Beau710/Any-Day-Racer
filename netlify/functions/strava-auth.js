c1101b75622f8e8df5c8921221ea6e7efb0e24a1
Beau710 <byeandle710@gmail.com>
2026-03-26 17:14:15 +1300

    Changes to code for netlify

---
+++ netlify/functions/strava-auth.js
@@ -0,0 +1,38 @@
+exports.handler = async function (event) {
+  const code = event.queryStringParameters.code;
+
+  if (!code) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "No code provided" }),
+    };
+  }
+
+  const response = await fetch("https://www.strava.com/oauth/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      client_id: process.env.STRAVA_CLIENT_ID,
+      client_secret: process.env.STRAVA_CLIENT_SECRET,
+      code: code,
+      grant_type: "authorization_code",
+    }),
+  });
+
+  const data = await response.json();
+
+  if (data.errors) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Strava auth failed" }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      access_token: data.access_token,
+      athlete: data.athlete,
+    }),
+  };
+};
