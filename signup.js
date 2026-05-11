4a55a1384c16d54a89dee65e55db5372d4d51261
Beau710 <byeandle710@gmail.com>
2026-05-11 21:07:16 +1200

    changed activity read on signup.js

--- signup.js
+++ signup.js
@@ -3,5 +3,5 @@ const redirectUri =
   "https://timely-shortbread-58de89.netlify.app/.netlify/functions/strava-auth";
 
 document.getElementById("strava-btn").addEventListener("click", function () {
-  window.location.href = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&approval_prompt=force&scope=read,activity:read`;
+  window.location.href = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&approval_prompt=force&scope=read_all,activity:read_all`;
 });
